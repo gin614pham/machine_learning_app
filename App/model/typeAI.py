da4d8bbf8348a90fd96f7a54e68a548718942b33
Wako <97005647+wako1203@users.noreply.github.com>
2024-04-22 06:56:16 +0700

    update train model

--- App/model/typeAI.py
+++ App/model/typeAI.py
@@ -1,6 +1,6 @@
 class typeAI:
     def __init__(self):
-        self.name = ["Linear Regression", "Logistic Regression", "KNN",]
+        self.name = ["Linear Regression", "Logistic Regression", "KNN", "Decision Tree"]
 
     def get_name(self):
         return self.name
